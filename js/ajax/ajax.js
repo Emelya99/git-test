fbf0b92722d68c14577e7c188f6acefd9c0709ea
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-06-25 13:30:56 +0300

    Added value list

--- js/ajax/ajax.js
+++ js/ajax/ajax.js
@@ -1,3 +1,19 @@
+const workList = document.querySelector('#work');
+const workBtn = document.querySelector('#work-btn');
+const workInput = document.querySelector('#work-input');
+workBtn.addEventListener('click',getInstagramItems);
 
-
-axios('https://625187db2dc339451d2ef136.mockapi.io/workItems');
+function getInstagramItems() {
+  if( workList.childNodes.length >= 1 ){
+    workList. innerHTML = '';
+  }
+  axios(`https://625187db2dc339451d2ef136.mockapi.io/instagramItems?page=1&limit=${workInput.value}`)
+  .then(function (response) {
+    itemsArr = response.data.items;
+    itemsArr.map(el => {
+      let li = document.createElement('li');
+      li.append(el.alt);
+      workList.appendChild(li);
+    } )
+  });
+}
