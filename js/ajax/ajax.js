8c073d8d964c83d9f050a7cb8e3d5002f431ecf7
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-06-27 21:15:24 +0300

    Updated first request

--- js/ajax/ajax.js
+++ js/ajax/ajax.js
@@ -1,5 +1,6 @@
 const workList = document.querySelector('#work');
 const workBtn = document.querySelector('#work-btn');
+const worktext = document.querySelector('#work-text');
 const workInput = document.querySelector('#work-input');
 workBtn.addEventListener('click',getInstagramItems);
 
@@ -9,7 +10,11 @@ function getInstagramItems() {
   }
   axios(`https://625187db2dc339451d2ef136.mockapi.io/instagramItems?page=1&limit=${workInput.value}`)
   .then(function (response) {
-    itemsArr = response.data.items;
+    let itemsArr = response.data.items;
+    let count = response.data.count;
+    if(workInput.value > count){
+      worktext.append(`Максимальное количество: ${count}`);
+    }
     itemsArr.map(el => {
       let li = document.createElement('li');
       li.append(el.alt);
