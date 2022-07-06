7f1dffc3f159d3b9e88ed1e7d39142ada6c8bae3
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-07-06 13:04:51 +0300

    Deleted trash scripts.js

--- js/scrpits.js
+++ js/scrpits.js
@@ -1,20 +1,3 @@
-// function bind(context, func) {
-//     return function(...args){
-//         func.apply(context,func)
-//     }
-// }
-
-// function logPerson() {
-//     console.log(`Person: ${this.name}, ${this.age}, ${this.job}`);
-// }
-
-// const person1 = {name: 'Jack', age: '22', job: 'Driver'}
-// const person2 = {name: 'Dimuch', age: '21', job: 'FrontEnd'}
-
-// bind(person1,logPerson)()
-// bind(person2,logPerson)()
-
-
 // Salary task
 const salaryInput = document.querySelector('#salary-input');
 const salaryBtn = document.querySelector('#salary-btn');
@@ -31,7 +14,6 @@ salaryBtn.addEventListener('click', () => {
         salaryText.innerHTML = "Число должно быть больше чем 0";
     } else{
         let res = Number(salaryInput.value);
-        // res += res / 100 * 15;
         alert("Добавка в виде премии в сумме 15% от зарплаты: " + (res += res / 100 * 15));
         alert("Налоги в сумме 10% от суммы всех начислений: " + (res -= res / 100 * 10));
         alert("Трата в магазине на сумму 190: " + (res -= 190));
